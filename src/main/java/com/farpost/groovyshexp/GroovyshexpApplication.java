22be22444e04e00c80866e697279f7552fb1077f
Daniel Demidko <demidko@farpost.com>
2020-12-20 15:50:52 +1000

    Старт проекта

---
+++ src/main/java/com/farpost/groovyshexp/GroovyshexpApplication.java
@@ -0,0 +1,13 @@
+package com.farpost.groovyshexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovyshexpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovyshexpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farpost/groovyshexp/GroovyshexpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farpost.groovyshexp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroovyshexpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
