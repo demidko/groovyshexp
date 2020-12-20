5e89754349dbb8df5300418b03079642592f7c2c
Daniel Demidko <demidko@farpost.com>
2020-12-20 16:40:55 +1000

    Gh service

--- src/main/java/com/farpost/groovyshexp/GroovyshexpApplication.java
+++ src/main/java/com/farpost/groovyshexp/GroovyshexpApplication.java
@@ -2,10 +2,23 @@ package com.farpost.groovyshexp;
 
 import static org.springframework.boot.SpringApplication.run;
 
+import lombok.val;
+import me.bazhenov.groovysh.spring.GroovyShellServiceBean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GroovyshexpApplication {
+
+  @Bean
+  GroovyShellServiceBean groovyShell() {
+    val gh = new GroovyShellServiceBean();
+    gh.setPublishContextBeans(true);
+    gh.setPort(6789);
+    gh.setDisableImportCompletions(true);
+    gh.setLaunchAtStart(true);
+    return gh;
+  }
 
   public static void main(String[] args) {
     run(GroovyshexpApplication.class, args);
